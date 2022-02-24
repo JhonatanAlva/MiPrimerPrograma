b3ef66111ba31f603c54ab62e9cb81e4da1ca8c7
JhonatanAlvarado <jloola@miumg.edu.gt>
2022-02-23 22:08:38 -0600

    aster

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class MiPrimerPrograma
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo facil, haz Click!");
+        }
+    }
+}
